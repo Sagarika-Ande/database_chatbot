438d9ec7a8ba009107244eaac75b7de51a8ce55b
Magendran.K <magendran.k@ril.com>
2025-10-22 15:18:14 +0530

    your commit message

--- test_openai.py
+++
@@ -1,9 +0,0 @@
-import openai
-
-openai.api_key = "REMOVEDproj-3U-RaRqTODFioNMPfKiAjQsMJptD_HtTqlJ9S4Y6u8V21p-XyZuqKvo7lhKUuQvViiCkB_ATbwT3BlbkFJyF9ceilXBVkpLKD-gaJzTTd4RXy1En5AeXx9jbzM5OnGs-8MSghRhi-5s45JO4O0uoFXfTOwEA"
-
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",  # Use GPT-3.5 instead of GPT-4
-    messages=[{"role": "user", "content": "Say hello"}]
-)
-print(response.choices[0].message.content)
