7a964b623d80eb7e4b43fa0eecf11300df05e1fc
Magendran.K <magendran.k@ril.com>
2025-10-22 15:20:19 +0530

    your commit message

--- config.py
+++ config.py
@@ -1,14 +1,16 @@
 import os
+from dotenv import load_dotenv
+
+# Load .env file
+load_dotenv()
 
 # ---------- DATABASE CONFIG ----------
 DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "Sagarika@28",
-    "database": "productsales"
+    "host": os.getenv("DB_HOST"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "database": os.getenv("DB_NAME")
 }
 
 # ---------- LLM CONFIG ----------
-# Example: OpenAI or Gemini (change as needed)
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "AIzaSyCYmU3IZyf8d_aJ7Q3OJbq66hO8Bz_gvyY")
-#OPENAI_API_KEY = os.getenv("OPENAI_API_KEY","REMOVEDproj-3U-RaRqTODFioNMPfKiAjQsMJptD_HtTqlJ9S4Y6u8V21p-XyZuqKvo7lhKUuQvViiCkB_ATbwT3BlbkFJyF9ceilXBVkpLKD-gaJzTTd4RXy1En5AeXx9jbzM5OnGs-8MSghRhi-5s45JO4O0uoFXfTOwEA")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
